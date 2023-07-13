799d844932a8d3be38bd3ed260460b56786af846
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-13 17:08:10 +0500

    Add association for like & update association for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     has_many :comments, foreign_key: 'post_id'
     has_many :likes, foreign_key: 'post_id'
-    belongs_to :author_id, class_name: 'User'
+    belongs_to :user, foreign_key: 'author_id'
 
 end
