9b547420b88ec8f42a9acfbf7c16a432b6d3debd
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-13 18:38:34 +0500

    Add method for post increment and decrement

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,17 @@ class Post < ApplicationRecord
     has_many :likes, foreign_key: 'post_id'
     belongs_to :user, foreign_key: 'author_id'
 
+    after_create :increment_post_counter
+    after_destroy :decrement_post_counter
+
+    def increment_post_counter
+      puts 'Incrementing post counter'
+      user.increment!(:post_counter)
+    end
+    
+    def decrement_post_counter
+      puts 'Decrementing post counter'
+      user.decrement!(:post_counter)
+    end
+
 end
