2bb81a6c1289ba097564da96ef1d45066fa57da8
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-13 18:44:06 +0500

    Add likes counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
     belongs_to :user, foreign_key: 'author_id'
     belongs_to :post, foreign_key: 'post_id'
+
+    def increment_like_counter
+      puts 'Incrementing like counter'
+      user.increment!(:likes_counter)
+    end
 end
