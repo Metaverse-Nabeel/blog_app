8065553815034052bebfb942535d7bca0e5cf146
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-14 20:25:55 +0500

    Correct linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'post_id'
 
-   after_save :increment_comment_counter
+  after_save :increment_comment_counter
 
   def increment_comment_counter
     puts 'Incrementing comment counter'
