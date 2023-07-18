928e5c466b717f0b93c1b4eaccd879aa02983600
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-18 17:53:52 +0500

    Removed wrong route added by mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/posts'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
     get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
     get '/users', to: 'users#index', as: 'users'
