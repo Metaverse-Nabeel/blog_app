7b18a6c64326255188a39f510b073ace2f1c2499
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-21 13:05:32 +0500

    Add posts and comments for the blog_app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,27 @@ class PostsController < ApplicationController
       format.json { render json: @post }
     end
   end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.LikesCounter = 0
+    @post.CommentsCounter = 0
+
+    if @post.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:Title, :Text)
+  end
 end
