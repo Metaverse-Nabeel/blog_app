2acee5aff858dd28a34fbb27e34b9bbfca6414e2
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-29 00:00:40 +0500

    Update the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  before_validation :set_default
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-
+  #Add code here
+  
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
@@ -19,8 +8,4 @@ class User < ApplicationRecord
     posts.order(created_at: :asc).limit(3).reverse
   end
 
-  def set_default
-    self.name = email.split('@')[0]
-    self.posts_counter = 0
-  end
 end
