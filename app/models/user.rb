56e3153a572d2aad430764b455de4fb0b14dc0c3
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-28 23:50:43 +0500

    Add migration of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   before_validation :set_default
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     # Defines the root path route ("/")
+    devise_for :users
     root 'users#index'
     get '/users'                   => 'users#index'
     get '/users/:id'               => 'users#show',as: 'user'
