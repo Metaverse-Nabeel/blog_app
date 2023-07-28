961e4eb4d487b54c85720423936a651c506a0668
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-28 21:59:09 +0500

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     # Defines the root path route ("/")
-  root 'users#index'
+    root 'users#index'
     get '/users'                   => 'users#index'
     get '/users/:id'               => 'users#show',as: 'user'
     get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
