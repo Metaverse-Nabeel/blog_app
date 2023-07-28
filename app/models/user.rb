9a8f78dcfbf2a7080e8545876d970d458ca23018
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-28 22:10:43 +0500

    New migration added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  before_validation :set_default
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
@@ -9,4 +14,10 @@ class User < ApplicationRecord
   def three_most_recent_posts
     posts.order(created_at: :asc).limit(3).reverse
   end
+
+  def set_default
+    self.name = email.split('@')[0]
+    self.posts_counter = 0
+  end
+  
 end
