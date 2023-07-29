fc22b272d1c64ba52884c0e640a42a28ac00b613
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-29 12:22:52 +0500

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
+
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :email, :password, :password_confirmation, :name, :bio)
