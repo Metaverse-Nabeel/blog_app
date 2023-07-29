f5f39e1a2c46f33a4dfec40b52bae2f9a4a66bea
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-29 12:20:58 +0500

    Make changes as per reviewers remarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    # Defines the root path route ("/")
-    devise_for :users
-    root 'users#index'
-    get '/users'                   => 'users#index'
-    get '/users/:id'               => 'users#show',as: 'user'
-    get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-    get '/users/:user_id/posts/new' => 'posts#new'
-    get '/users/:user_id/posts/:post_id' => 'posts#show',  as: 'post'
-    post '/users/:id/posts/create', to: 'posts#create'
-    get '/users/:id/posts/:post_id/comments/new', to: 'comments#new'
-    post '/users/:id/posts/:post_id/comments/create', to: 'comments#create'
-    post '/users/:id/posts/:id/likes/create', to: 'likes#create'
-  # Defines the root path route ("/")
+  # Devise routes should be placed above other routes to avoid conflicts
+  devise_for :users
 
-  resources :users do
-    resources :posts do
-      resources :comments
-      post 'likes', to: 'likes#create' # Nested route for creating a like
+  # Define your other routes here...
+
+  root 'users#index'
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      post 'likes', to: 'likes#create', on: :member # Nested route for creating a like
     end
   end
+
+  # Your other routes here...
 end
