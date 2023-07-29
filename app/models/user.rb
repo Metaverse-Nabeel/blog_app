0ff0859c8288ad3ccba6c6745215791528f1b4db
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-29 12:03:21 +0500

    Removed duplication of devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   before_validation :set_default
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
